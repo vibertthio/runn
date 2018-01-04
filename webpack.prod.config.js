5cb1b5977656a5684fd3247965da9a114926dc83
Vibert Thio <vibertthio@gmail.com>
2018-01-04 15:54:41 +0800

    :fire: remove source map of prod config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -7,11 +7,9 @@ const common = require('./webpack.common.config.js');
 
 module.exports = merge(common, {
 	plugins: [
-		new UglifyJSPlugin({
-			sourceMap: true
+		new UglifyJSPlugin(),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production'),
 		}),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
 	],
 });
